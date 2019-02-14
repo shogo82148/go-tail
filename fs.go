5cf27ed929ca08f94d564c52a7b48d4ef263663e
Ichinose Shogo <shogo82148@gmail.com>
2019-02-14 22:27:24 +0900

    add links to fluent-bit

--- fs.go
+++ fs.go
@@ -7,6 +7,7 @@ import (
 	"os"
 )
 
+// from https://github.com/fluent/fluent-bit/blob/2b80bb64c3feb9979126c13f4409ce10afd8b23e/plugins/in_tail/tail_file.c#L914-L963
 func getFileName(f *os.File) (string, error) {
 	fd := f.Fd()
 	return os.Readlink(fmt.Sprintf("/proc/%d/fd/%d", os.Getpid(), fd))
--- fs_darwin.go
+++ fs_darwin.go
@@ -10,6 +10,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// from https://github.com/fluent/fluent-bit/blob/2b80bb64c3feb9979126c13f4409ce10afd8b23e/plugins/in_tail/tail_file.c#L914-L963
 func getFileName(f *os.File) (string, error) {
 	buf := make([]byte, 2048)
 	data := (*reflect.StringHeader)(unsafe.Pointer(&buf)).Data
