9dfbceb80f51e2a2e5594232880093b3f9092631
Ichinose Shogo <shogo82148@gmail.com>
2021-03-14 18:06:53 +0900

    introduce xerrors

--- fs.go
+++ fs.go
@@ -10,5 +10,9 @@ import (
 // from https://github.com/fluent/fluent-bit/blob/2b80bb64c3feb9979126c13f4409ce10afd8b23e/plugins/in_tail/tail_file.c#L914-L963
 func getFileName(f *os.File) (string, error) {
 	fd := f.Fd()
-	return os.Readlink(fmt.Sprintf("/proc/%d/fd/%d", os.Getpid(), fd))
+	name, err := os.Readlink(fmt.Sprintf("/proc/%d/fd/%d", os.Getpid(), fd))
+	if err != nil {
+		return "", fmt.Errorf("tail: fail to get path of fd: %d", fd)
+	}
+	return name, nil
 }
